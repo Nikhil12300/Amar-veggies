6cf6e6fc4e764e3f35bea0ed7c3234cde2da62f1
Nikhil Sonkar <nikhilsonkar12300@gmail.com>
2026-05-09 18:36:49 +0530

    fix sqlalchemy postgres driver

--- server.py
+++ server.py
@@ -39,13 +39,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./amar_veggies.db")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace(
         "postgres://",
-        "postgresql+psycopg://",
-        1
-    )
-elif DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace(
         "postgresql://",
-        "postgresql+psycopg://",
         1
     )
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "")
