be13361a67c89e09a6f66d17ac6dc34a25a21a85
Nikhil Sonkar <nikhilsonkar12300@gmail.com>
2026-05-13 13:21:58 +0530

    debug push notifications

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD-NLbMzqg9lLQoSIslDMHsufyOtTE_gOs",
+  authDomain: "amar-veggies-a3f2a.firebaseapp.com",
+  projectId: "amar-veggies-a3f2a",
+  storageBucket: "amar-veggies-a3f2a.firebasestorage.app",
+  messagingSenderId: "522883626327",
+  appId: "1:522883626327:web:c53a7ce54701ebbadafa12",
+  measurementId: "G-NZQ83J6LDF"
 });
 
 const messaging = firebase.messaging();
